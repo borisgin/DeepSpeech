fixed bug in seq_len computation for stride > 2 added batch_norm to conv layers

DeepSpeech2.py
@@ -125,14 +125,14 @@ tf.app.flags.DEFINE_integer ('summary_secs',     None,        'interval in secon
 tf.app.flags.DEFINE_integer ('summary_steps',    None,        'interval in steps for saving TensorBoard summaries - if 0, no summaries will be written')
 
 # Geometry
-tf.app.flags.DEFINE_string  ('input_type',      'mfcc',       'input features type: mfcc or spectrogram')
-tf.app.flags.DEFINE_integer ('num_audio_features',        32,          'number of mfcc coefficients or spectrogram frequency bins')
+tf.app.flags.DEFINE_string  ('input_type',         'spectrogram',       'input features type: mfcc or spectrogram')
+tf.app.flags.DEFINE_integer ('num_audio_features',        161,          'number of mfcc coefficients or spectrogram frequency bins')
 
 
 # TODO: input type: mfcc or spectrogram
 
 tf.app.flags.DEFINE_integer ('num_conv_layers',  2,            'layer width to use when initialising layers')
-tf.app.flags.DEFINE_integer ('num_rnn_layers',   2,            'layer width to use when initialising layers')
+tf.app.flags.DEFINE_integer ('num_rnn_layers',   1,            'layer width to use when initialising layers')
 tf.app.flags.DEFINE_string  ('rnn_type',        'gru',         'rnn-cell type')
 tf.app.flags.DEFINE_string  ('rnn_cell_dim',     1024,         'rnn-cell dim')
 
@@ -411,7 +411,37 @@ class CustomRNNCell2(tf.contrib.rnn.BasicRNNCell):
             #output = relux(res, capping=20)
         return output, output
 
-# ----------------------------------------------------------------
+# --------------------------------------------------------------------
+
+def batch_norm(name,
+               input,
+               training = True):
+    n_channels = input.get_shape()[-1]
+    gamma =  0.95
+    beta = variable_on_worker_level(name + '.beta',  shape=[n_channels],
+                                    initializer=tf.zeros_initializer)
+    gamma = variable_on_worker_level(name + '.gamma',  shape=[n_channels],
+                                   initializer=tf.ones_initializer)
+    g_mean = variable_on_worker_level(name + '.g_mean', shape=[n_channels],
+                                      initializer=tf.zeros_initializer,
+                                      trainable=False)
+    g_var = variable_on_worker_level(name + '.g_var', shape=[n_channels],
+                                     initializer=tf.ones_initializer,
+                                     trainable=False)
+    if (training):
+        batch_mean, batch_var = tf.nn.moments(input, [0, 1, 2])
+        g_mean = g_mean * gamma + batch_mean * (1 - gamma)
+        g_var  = g_var * gamma + batch_mean * (1 - gamma)
+        mean = batch_mean
+        var  = batch_var
+    else:
+        mean = g_mean
+        var  = g_var
+
+    normed = tf.nn.batch_normalization(input, mean, var, beta, gamma, 1e-3)
+    return normed
+
+
 def conv2D(name,
            input,
            in_channels = 1,
@@ -421,7 +451,8 @@ def conv2D(name,
            padding='SAME',
            activation_fn=lambda x: tf.minimum(tf.nn.relu(x), FLAGS.relu_clip),
            weights_initializer= tf.contrib.layers.xavier_initializer(uniform=False),
-           bias_initializer=tf.zeros_initializer()
+           bias_initializer=tf.zeros_initializer(),
+           training = True
            ):
     #filter_shape= [kernel_size[0], kernel_size[1], in_channels, output_channels]
     w = variable_on_worker_level(name+'.w',
@@ -433,6 +464,7 @@ def conv2D(name,
 
     s = [1, strides[0], strides[1], 1]
     y = tf.nn.conv2d(input, w, s, padding)
+    y = batch_norm(name, y , training = training)
     y = tf.nn.bias_add(y, b)
     output = activation_fn(y)
     return output
@@ -445,7 +477,8 @@ def rnn_cell(rnn_cell_dim = 1024, layer_type="gru"):
                        if 'reuse' not in inspect.getargspec(tf.contrib.rnn.BasicLSTMCell.__init__).args else \
                        tf.contrib.rnn.BasicLSTMCell(rnn_cell_dim, forget_bias=1.0, state_is_tuple=True,
                                                 reuse=tf.get_variable_scope().reuse)
-        cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob= dropout, output_keep_prob= dropout,
+        if (dropout < 1.0):
+            cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob= dropout, output_keep_prob= dropout,
                                                      seed=FLAGS.random_seed)
     elif (layer_type=="layernorm_lstm"):
          cell = tf.contrib.rnn.LayerNormBasicLSTMCell(rnn_cell_dim, forget_bias=1.0,
@@ -456,9 +489,10 @@ def rnn_cell(rnn_cell_dim = 1024, layer_type="gru"):
                                                       dropout_keep_prob=dropout, dropout_prob_seed=FLAGS.random_seed,
                                                       reuse=tf.get_variable_scope().reuse)
     elif (layer_type=="gru"):
-            cell = tf.contrib.rnn.GRUCell(rnn_cell_dim) \
-                if 'reuse' not in inspect.getargspec(tf.contrib.rnn.GRUCell.__init__).args else \
-                tf.contrib.rnn.GRUCell(rnn_cell_dim, reuse=tf.get_variable_scope().reuse)
+        cell = tf.contrib.rnn.GRUCell(rnn_cell_dim) \
+            if 'reuse' not in inspect.getargspec(tf.contrib.rnn.GRUCell.__init__).args else \
+            tf.contrib.rnn.GRUCell(rnn_cell_dim, reuse=tf.get_variable_scope().reuse)
+        if (dropout < 1.0):
             cell = tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=dropout, output_keep_prob=dropout,
                                          seed=FLAGS.random_seed)
 
@@ -467,7 +501,7 @@ def rnn_cell(rnn_cell_dim = 1024, layer_type="gru"):
 
 #===========================================================
 
-def DeepSpeech2(batch_x, seq_length, dropout):
+def DeepSpeech2(batch_x, seq_length,training):
     r'''
     DeepSpeech2-like model https://arxiv.org/pdf/1512.02595.pdf
     The variables named ``layer_name/w``, hold the learned weights.
@@ -504,7 +538,7 @@ def DeepSpeech2(batch_x, seq_length, dropout):
     # Convolutional layers configuration
     conv_layers = [
         {'kernel_size': [11,41], 'stride': [2,2], 'num_channels': 32, 'padding': 'SAME' },
-        {'kernel_size': [11,21], 'stride': [1,2], 'num_channels': 64, 'padding': 'SAME' },
+        {'kernel_size': [11,21], 'stride': [2,2], 'num_channels': 64, 'padding': 'SAME' },
         {'kernel_size': [11,21], 'stride': [1,2], 'num_channels': 96, 'padding': 'SAME' }
     ]
     # Number of convolutional num_conv_layers
@@ -524,8 +558,8 @@ def DeepSpeech2(batch_x, seq_length, dropout):
             seq_length = (seq_length - kernel_size[0] + strides[0]) // strides[0]
             f_out = (f_out - kernel_size[1]+strides[1]) // strides[1]
         else:
-            seq_length = seq_length // strides[0]
-            f_out = f_out // strides[1]
+            seq_length = (seq_length + strides[0]-1) // strides[0]
+            f_out = (f_out + strides[1] -  1) // strides[1]
 
         print('{}: kernel={} stride={} ch=[{}, {}] f_out={}'.format(
             name, kernel_size, strides, ch_in, ch_out, f_out))
@@ -537,7 +571,8 @@ def DeepSpeech2(batch_x, seq_length, dropout):
             weights_initializer=tf.contrib.layers.xavier_initializer(uniform=False),
             # weights_initializer=tf.contrib.layers.variance_scaling_initializer(),
             # bias_initializer=tf.random_normal_initializer(stddev=0.00001)
-            bias_initializer = tf.constant_initializer(0.000001)
+            bias_initializer = tf.constant_initializer(0.000001),
+                      training = training
         )
     
     #--- FC layers -----
@@ -626,7 +661,7 @@ def decode_with_lm(inputs, sequence_length, beam_width=128,
 # Conveniently, this loss function is implemented in TensorFlow.
 # Thus, we can simply make use of this implementation to define our loss.
 
-def calculate_mean_edit_distance_and_loss(model_feeder, tower, dropout):
+def calculate_mean_edit_distance_and_loss(model_feeder, tower, training):
     r'''
     This routine beam search decodes a mini-batch and calculates the loss and mean edit distance.
     Next to total and average loss it returns the mean edit distance,
@@ -638,7 +673,7 @@ def calculate_mean_edit_distance_and_loss(model_feeder, tower, dropout):
     # Calculate the logits of the batch using BiRNN
  #   logits, batch_seq_len = DeepSpeech2(batch_x, tf.to_int64(batch_seq_len), dropout)
  #   logits, batch_seq_len = DeepSpeech2(batch_x, tf.to_int32(batch_seq_len), dropout)
-    logits, batch_seq_len = DeepSpeech2(batch_x, batch_seq_len, dropout)
+    logits, batch_seq_len = DeepSpeech2(batch_x, batch_seq_len, training=training)
 
     # Compute the CTC loss using either TensorFlow's `ctc_loss` or Baidu's `warp_ctc_loss`.
     if FLAGS.use_warpctc:
@@ -758,7 +793,7 @@ def get_tower_results(model_feeder, optimizer):
                     # Calculate the avg_loss and mean_edit_distance and retrieve the decoded
                     # batch along with the original batch's labels (Y) of this tower
                     total_loss, avg_loss, distance, mean_edit_distance, decoded, labels = \
-                        calculate_mean_edit_distance_and_loss(model_feeder, i, dropout=1.0  if optimizer is None else dropout)
+                        calculate_mean_edit_distance_and_loss(model_feeder, i, training = (optimizer is None))
 
                     # Allow for variables to be re-used by the next tower
                     tf.get_variable_scope().reuse_variables()
@@ -1622,7 +1657,7 @@ def train(server=None):
                                tower_feeder_count=len(available_devices),
                                input_type=input_type)
 
-    if (FLAGS.decay_steps > 0):
+    if (FLAGS.decay_steps > 0) and (FLAGS.decay_rate > 0):
         lr = tf.train.exponential_decay(learning_rate = FLAGS.learning_rate,
                                              global_step   = global_step,
                                              decay_steps   = FLAGS.decay_steps,
@@ -1818,7 +1853,7 @@ def create_inference_graph(batch_size=None, output_is_logits=False, use_new_deco
     seq_length = tf.placeholder(tf.int32, [batch_size], name='input_lengths')
 
     # Calculate the logits of the batch using BiRNN
-    logits = DeepSpeech2(input_tensor, tf.to_int64(seq_length) if FLAGS.use_seq_length else None, dropout=-1.0)
+    logits = DeepSpeech2(input_tensor, tf.to_int64(seq_length) if FLAGS.use_seq_length else None)
 
     if output_is_logits:
         return logits

util/audio.py
@@ -20,8 +20,8 @@ except ImportError:
         if DeprecationWarning.displayed is not True:
             DeprecationWarning.displayed = True
             print('------------------------------------------------------------------------', file=sys.stderr)
-            print('WARNING: libdeepspeech failed to load, resorting to deprecated code',      file=sys.stderr)
-            print('         Refer to README.md for instructions on installing libdeepspeech', file=sys.stderr)
+            print('WARNING: using python_speech_features: type {} num_featues {} '.format(input_type, numcep),
+                  file=sys.stderr)
             print('------------------------------------------------------------------------', file=sys.stderr)
 
         '''
@@ -90,6 +90,7 @@ except ImportError:
         # Whiten inputs (TODO: Should we whiten?)
         m = np.mean(train_inputs)
         v = np.std(train_inputs)
+        train_inputs = np.copy(train_inputs)
         train_inputs = (train_inputs - m) / v
 
         # Return results