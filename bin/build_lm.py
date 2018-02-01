303dec32744886934a322ce65ce554d7403102cc
Vitaly Lavrukhin <vlavrukhin>
2018-02-01 20:32:27 +0000

    Minor refactoring of build LM script

--- bin/build_lm.py
+++ bin/build_lm.py
@@ -4,21 +4,27 @@ import argparse
 
 KENLM_BIN = '/opt/kenlm/build/bin/'
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Build N-gram LM model from CSV files')
-    parser.add_argument('csv', metavar='csv', type=str, nargs='+', help='DeepSpeech CSV file')
-    parser.add_argument('--N', type=int, help='N for N-grams', default=3)
-    args = parser.parse_args()
-
-
+def get_corpus(csv_files):
+    '''
+    Get text corpus from a list of CSV files
+    '''
     SEP = '\n'
     corpus = ''
-    for f in args.csv:
+    for f in csv_files:
         df = pd.read_csv(f)
         corpus += SEP.join(df['transcript']) + SEP
     # remove the last SEP
     corpus = corpus[:-1]
+    return corpus
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build N-gram LM model from CSV files')
+    parser.add_argument('csv', metavar='csv', type=str, nargs='+', help='DeepSpeech CSV file')
+    parser.add_argument('--n', type=int, help='n for n-grams', default=3)
+    args = parser.parse_args()
+
+    corpus = get_corpus(args.csv)
 
     path_prefix, _ = os.path.splitext(args.csv[0])
     corpus_name = path_prefix + '.txt'
@@ -28,12 +34,17 @@ if __name__ == '__main__':
     with open(corpus_name, 'w') as f:
         f.write(corpus)
 
+    command = KENLM_BIN + 'lmplz --text {} --arpa {} --o {}'.format(
+        corpus_name, arpa_name, args.n)
+    print(command)
+    os.system(command)
 
-    os.system(KENLM_BIN + 'lmplz --text {} --arpa {} --o {}'.format(
-        corpus_name, arpa_name, args.N))
+    command = KENLM_BIN + 'build_binary -s {} {}'.format(
+        arpa_name, lm_name)
+    print(command)
+    os.system(command)
 
-    os.system(KENLM_BIN + 'build_binary -s {} {}'.format(
-        arpa_name, lm_name))
-
-    os.system('native_client/generate_trie data/alphabet.txt {} {} {}'.format(
-        lm_name, corpus_name, trie_name))
+    command = 'native_client/generate_trie data/alphabet.txt {} {} {}'.format(
+        lm_name, corpus_name, trie_name)
+    print(command)
+    os.system(command)
