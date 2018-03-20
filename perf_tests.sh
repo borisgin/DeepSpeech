35829cc3f7a2d6de51687c5c07c9049bbbe7f604
Vitaly Lavrukhin <vlavrukhin>
2018-03-20 00:15:01 +0000

    Fixed typo

--- perf_tests.sh
+++ perf_tests.sh
@@ -4,7 +4,7 @@ for n in 2 3 5 ; do
   for h in 3200 ; do
     for bs in 8 32 64 128 256 ; do
       for sl in 128 256 512 ; do
-        NUM_RNN_LAYERS=$n HIDDEN_SIZE=$h BATCH_SIZE=$bs SEQ_LEN=$sl NUM_ITERS=$N_ITERS bin/perftest_gpu.sh
+        NUM_RNN_LAYERS=$n HIDDEN_SIZE=$h BATCH_SIZE=$bs SEQ_LEN=$sl NUM_ITERS=$N_ITERS bin/perftest_1gpu.sh
         NUM_RNN_LAYERS=$n HIDDEN_SIZE=$h BATCH_SIZE=$bs SEQ_LEN=$sl NUM_ITERS=$N_ITERS bin/perftest_cpu.sh
       done
     done
